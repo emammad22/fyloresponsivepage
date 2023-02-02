f9b2f7a8b63042117237cc45e1eb9e0be3069323
emammad22 <mammadoveltun22@gmail.com>
2023-02-02 19:17:48 +0400

    solved some design problems for validation

--- scripts/validation.js
+++ scripts/validation.js
@@ -2,15 +2,15 @@ var email = document.getElementById('mail');
 var submit = document.getElementById('submit');
 var label = document.getElementById('validate');
 
-var mailFormat =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-submit.addEventListener('click',(e)=>{
-    if(email.value.match(mailFormat)){
-        if(email.classList.contains('error') && label.classList.contains('validation-error')){
-        email.classList.remove('error');
-        label.classList.remove('validation-error');
-    }
-    }else{
+submit.addEventListener('click', (e) => {
+    if (email.value.match(mailFormat)) {
+        if (email.classList.contains('error') && label.classList.contains('validation-error')) {
+            email.classList.remove('error');
+            label.classList.remove('validation-error');
+        }
+    } else {
         email.classList.add('error');
         label.classList.add('validation-error');
     }
